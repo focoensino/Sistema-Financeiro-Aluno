8f3a52fde46f5c448ef9449782840722ed9182c9
Sérgio Tafine <focoensinoonline@gmail.com>
2023-01-21 10:10:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,9 @@ function onChangePassword() {
     togglePasswordErrors();
 }
 
-function login() {
-    window.location.href = "pages/home/home.html";
-}
 
-function register() {
-    window.location.href = "pages/register/register.html";
-}
+
+//inserir o location
 
 function toggleEmailErrors() {
     const email = form.email().value;
